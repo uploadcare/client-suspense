532c5c358629c436102e5842cb1ac013670421f8
jeetiss <jeetiss@yandex.ru>
2019-08-08 09:18:33 +0300

    simplify babel config and npm scripts

--- .babelrc.js
+++ .babelrc.js
@@ -1,11 +1,7 @@
-const TESTS = process.env.NODE_ENV === 'test'
-
 module.exports = function(api) {
   api.cache(true)
 
   return {
-    presets: [TESTS && '@babel/preset-env', '@babel/preset-react'].filter(
-      Boolean
-    )
+    presets: ['@babel/preset-env', '@babel/preset-react']
   }
 }
