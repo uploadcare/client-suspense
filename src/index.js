b79db2764c80bffe0d730ab06a09ef16ec46e093
daydarkln <daydarkez@gmail.com>
2019-10-30 18:01:09 +0300

    fix: es6 to es5 (#15)
    * downgrade es6 destruct for react hook
    * downgrader to es5 completed
    * hook to state changed
    * useEffect argument bug fixed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import { createElement as h, lazy, Suspense, useState, useEffect } from 'react'
 
-const ClientSuspense = props => {
-  const [isClient, setClient] = useState(false)
-  useEffect(() => setClient(true), [])
+function ClientSuspense(props) {
+  var state = useState(false),
+    isClient = state[0],
+    setClient = state[1];
 
-  return isClient ? h(Suspense, props) : props.fallback
+  useEffect(function() {
+    setClient(true);
+  }, [])
+
+  return isClient ? h(Suspense, props) : props.fallback;
 }
 
 export { lazy, ClientSuspense as Suspense }
