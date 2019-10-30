d3016bcd5a0bf57cb4906afccc85cb9a4a41a778
daydarkln <daydarkez@gmail.com>
2019-10-30 15:55:32 +0300

    downgrade es6 destruct for react hook

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import { createElement as h, lazy, Suspense, useState, useEffect } from 'react'
 
 const ClientSuspense = props => {
-  const [isClient, setClient] = useState(false)
+  const hook = useState(false);
+  const isClient = hook[0];
+  const setClient = hook[1];
   useEffect(() => setClient(true), [])
 
   return isClient ? h(Suspense, props) : props.fallback
